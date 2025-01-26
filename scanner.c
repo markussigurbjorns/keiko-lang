7e5cf4fb1ab8e3aee2d550da76c1dcf7df29a8ab
markussigurbjorns <markussigurbjorns@gmail.com>
2025-01-26 21:04:32 +0100

    expression evaluation

--- scanner.c
+++ scanner.c
@@ -111,11 +111,12 @@ static TokenType identifierType() {
         case 'f' : 
             if (scanner.current - scanner.start > 1) {
                 switch (scanner.start[1]) {
-                    case 'a' : return checkKeyword(1,3,"lse", TOKEN_FALSE);
-                    case 'o' : return checkKeyword(1,1,"r", TOKEN_FOR);
-                    case 'u' : return checkKeyword(1,1,"n", TOKEN_FUN);
+                    case 'a' : return checkKeyword(2,3,"lse", TOKEN_FALSE);
+                    case 'o' : return checkKeyword(2,1,"r", TOKEN_FOR);
+                    case 'u' : return checkKeyword(2,1,"n", TOKEN_FUN);
                 }
             }
+            break;
         case 'i' : return checkKeyword(1,1,"f", TOKEN_IF);
         case 'n' : return checkKeyword(1,2,"il", TOKEN_NIL);
         case 'o' : return checkKeyword(1,1,"r", TOKEN_OR);
@@ -125,10 +126,11 @@ static TokenType identifierType() {
         case 't' :
             if (scanner.current - scanner.start > 1) {
                 switch (scanner.start[1]) {
-                    case 'h' : return checkKeyword(1,2,"is", TOKEN_THIS);
-                    case 'r' : return checkKeyword(1,2,"ue", TOKEN_TRUE);
+                    case 'h' : return checkKeyword(2,2,"is", TOKEN_THIS);
+                    case 'r' : return checkKeyword(2,2,"ue", TOKEN_TRUE);
                 }
             }
+            break;
         case 'v' : return checkKeyword(1,2,"ar", TOKEN_VAR);
         case 'w' : return checkKeyword(1,2,"hile", TOKEN_WHILE);
     }
