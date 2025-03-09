68ba304d4f2a5d14d0d1dfdb32a533754b993186
markussigurbjorns <markussigurbjorns@gmail.com>
2025-03-10 00:41:50 +0100

    jump and control flow - memory issues

--- scanner.c
+++ scanner.c
@@ -136,7 +136,7 @@ static TokenType identifierType() {
             }
             break;
         case 'v' : return checkKeyword(1,2,"ar", TOKEN_VAR);
-        case 'w' : return checkKeyword(1,2,"hile", TOKEN_WHILE);
+        case 'w' : return checkKeyword(1,4,"hile", TOKEN_WHILE);
     }
     return TOKEN_IDENTIFIER;
 }
